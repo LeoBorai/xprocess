764eb1ed4a4080c4eabcdd580c6ffd23cd5841e3
Leo Borai <estebanborai@gmail.com>
2025-04-14 20:33:49 -0400

    feat: `spawn_with_args` method (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,50 @@ use std::process::{Command, Stdio};
 use anyhow::{Result, bail};
 
 /// Reference of a system process spawned by [`Process::spawn`]
+///
+/// # Example
+///
+/// ```ignore
+/// use xprocess::Process;
+///
+/// fn main() {
+///    let process = Process::spawn("sleep").expect("Failed to spawn process");
+///    println!("Spawned process with PID: {}", process.pid());
+///    process.kill().expect("Failed to kill process");
+/// }
+/// ```
+///
 pub struct Process {
     pid: u32,
 }
 
 impl Process {
     pub fn spawn<S: AsRef<OsStr>>(cmd: S) -> Result<Self> {
-        let mut child = Command::new(cmd);
+        let mut command = Self::build_command(cmd, []);
+        Self::spawn_child_process(&mut command)
+    }
 
-        let mut child = child
+    pub fn spawn_with_args<S, I>(cmd: S, args: I) -> Result<Self>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        let mut command = Self::build_command(cmd, args);
+        Self::spawn_child_process(&mut command)
+    }
+
+    fn build_command<S, I>(cmd: S, args: I) -> Command
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<OsStr>,
+    {
+        let mut command = Command::new(cmd);
+        command.args(args);
+        command
+    }
+
+    fn spawn_child_process(cmd: &mut Command) -> Result<Self> {
+        let mut child = cmd
             .stdin(Stdio::null())
             .stdout(Stdio::null())
             .stderr(Stdio::null());
@@ -51,5 +86,31 @@ impl Process {
                 bail!("Error executing kill command: {}", e);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::thread;
+    use std::time::Duration;
+
+    use super::*;
+
+    #[test]
+    fn spawn_process() {
+        let process = Process::spawn("sleep").expect("Failed to spawn process");
+        assert!(process.pid() > 0);
+        thread::sleep(Duration::from_millis(100));
+        let result = process.kill();
+        assert!(result.is_ok(), "Failed to kill the process");
+    }
+
+    #[test]
+    fn spawn_process_with_args() {
+        let process = Process::spawn_with_args("sleep", ["1"]).expect("Failed to spawn process");
+        assert!(process.pid() > 0);
+        thread::sleep(Duration::from_millis(100));
+        let result = process.kill();
+        assert!(result.is_ok(), "Failed to kill the process");
     }
 }
